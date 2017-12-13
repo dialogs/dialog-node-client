1d6279af85cd53456af98f24f5c1851697797109
Nikita Gusakov <gusnkt@gmail.com>
2017-12-13 16:46:46 +0300

    Revert "feat: add more bot functionality"
    This reverts commit a5361fa942a9625e41ace4a6b8a17e8577d2bec9.

--- src/Bot.js
+++ src/Bot.js
@@ -4,51 +4,41 @@
 
 const EventEmitter = require('events');
 const createClient = require('./client');
-const ResolveMessageQueue = require('./ResolveMessageQueue');
 
 class Bot extends EventEmitter {
   constructor(options) {
     super();
 
-    this.ready = this.setup(options);
-  }
-
-  /**
-   * @private
-   */
-  async setup(options) {
-    const messenger = await createClient({
+    this.ready = createClient({
       quiet: options.quiet,
       endpoints: options.endpoints
+    }).then((messenger) => {
+      return new Promise((resolve, reject) => {
+        const onSuccess = () => resolve(messenger);
+        const onError = (tag, message) => reject(new Error(message, tag));
+
+        if (typeof options.phone === 'string' && typeof options.code === 'string') {
+          messenger.requestSms(
+            options.phone,
+            () => messenger.sendCode(options.code, onSuccess, onError)
+          );
+        } else if (typeof options.username === 'string' && typeof options.password === 'string') {
+          messenger.startUserNameAuth(
+            options.username,
+            () => messenger.sendPassword(options.password, onSuccess, onError),
+            onError
+          );
+        } else {
+          throw new Error('Auth credentials not defined');
+        }
+      }).then((messenger) => {
+        messenger.onUpdate((update) => {
+          this.emit(update.type, update.payload);
+        });
+
+        return messenger;
+      });
     });
-
-    await new Promise((resolve, reject) => {
-      const onSuccess = () => resolve(messenger);
-      const onError = (tag, message) => reject(new Error(message, tag));
-
-      if (typeof options.phone === 'string' && typeof options.code === 'string') {
-        messenger.requestSms(
-          options.phone,
-          () => messenger.sendCode(options.code, onSuccess, onError)
-        );
-      } else if (typeof options.username === 'string' && typeof options.password === 'string') {
-        messenger.startUserNameAuth(
-          options.username,
-          () => messenger.sendPassword(options.password, onSuccess, onError),
-          onError
-        );
-      } else {
-        throw new Error('Auth credentials not defined');
-      }
-    });
-
-    this.messageQueue = new ResolveMessageQueue(messenger);
-
-    messenger.onUpdate((update) => {
-      this.emit(update.type, update.payload);
-    });
-
-    return messenger;
   }
 
   onAsync(eventName, callback) {
@@ -58,20 +48,7 @@ class Bot extends EventEmitter {
   }
 
   onMessage(callback) {
-    this.onAsync('MESSAGE_ADD', async ({ peer, mid, sender }) => {
-      const messenger = await this.ready;
-      if (sender === messenger.getUid()) {
-        return;
-      }
-
-      this.messageQueue.add(peer, mid, (error, message) => {
-        if (error) {
-          this.emit('error', error);
-        } else if (message) {
-          callback(peer, message).catch(error => this.emit('error', error));
-        }
-      });
-    });
+    this.onAsync('MESSAGE_ADD', callback);
   }
 
   onInteractiveEvent(callback) {
@@ -88,19 +65,9 @@ class Bot extends EventEmitter {
     return messenger.getUid();
   }
 
-  async getUser(uid) {
+  async sendTextMessage(peer, text) {
     const messenger = await this.ready;
-    return messenger.getUser(uid);
-  }
-
-  async getGroup(gid) {
-    const messenger = await this.ready;
-    return messenger.getUser(gid);
-  }
-
-  async sendTextMessage(peer, text, attach) {
-    const messenger = await this.ready;
-    messenger.sendMessage(peer, text, attach);
+    messenger.sendMessage(peer, text);
   }
 
   async editTextMessage(peer, rid, text) {
@@ -115,15 +82,8 @@ class Bot extends EventEmitter {
 
   async editInteractiveMessage(peer, rid, text, actions) {
     const messenger = await this.ready;
-    messenger.sendInteractiveMessage(peer, rid, text, actions);
+    await messenger.editInteractiveMessage(peer, rid, text, actions);
   }
-
-  async readChat(peer) {
-    const messenger = await this.ready;
-    messenger.onConversationOpen(peer);
-    messenger.onConversationClosed(peer);
-  }
-
 
   async sendFileMessage(peer, fileName) {
     const messenger = await this.ready;
